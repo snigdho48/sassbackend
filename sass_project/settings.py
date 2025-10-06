308ea3fc3c00eaa0e6769eaae0efdf323ae58eb3
Atiquzzaman Snigdho <www.atik48@gmail.com>
2025-10-06 12:13:32 +0600

    added.

--- sass_project/settings.py
+++ sass_project/settings.py
@@ -187,10 +187,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = BASE_DIR / 'staticfiles',
 
 # Media files
 MEDIA_URL = '/media/'
